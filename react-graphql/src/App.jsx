31b6eb3ca9d8db99f90ae4dcbbb1a53a7ae7da93
francogb98 <francobaudino98@gmail.com>
2023-07-25 02:44:51 -0300

    ssss

--- react-graphql/src/App.jsx
+++ react-graphql/src/App.jsx
@@ -1,12 +1,3 @@
-// import reactLogo from "./assets/react.svg";
-// import Persons from "./Persons";
-// import viteLogo from "/vite.svg";
-// import "./App.css";
-// import PersonForm from "./PersonForm";
-// import Notify from "./Notify";
-// import { usePerson } from "./persons/customhooks";
-// import { useState } from "react";
-// import PhoneForm from "./PhoneForm";
 import { useApolloClient } from "@apollo/client";
 import LoginForm from "./loginForm/LoginForm";
 import Confirm from "./loginForm/Confirm";
@@ -20,18 +11,7 @@ import Inscripcion from "./HomeUserPublic/Inscripcion";
 import Perfil from "./HomeUserPublic/Perfil/Perfil";
 
 function App() {
-  // const { data, error, loading } = usePerson();
-  // const [errorMessage, setErrorMessage] = useState(null);
-  // const [token, setToken] = useState(() =>
-  //   localStorage.getItem("phonenumbers-user-token")
-  // );
   const client = useApolloClient();
-
-  // const logout = () => {
-  //   setToken(null);
-  //   localStorage.clear();
-  //   client.resetStore();
-  // };
 
   return (
     <div className="container-fluid">
@@ -58,42 +38,6 @@ function App() {
           </Route>
         </Routes>
       </div>
-
-      {/* <Notify errorMessage={errorMessage} /> */}
-      {/* <header>
-        {token ? (
-          <div style={{ width: "800px" }}>
-            <button onClick={logout}>Cerrar Sesion</button>
-
-            <div>
-              {loading ? (
-                <p>Cargando...</p>
-              ) : (
-                <>
-                  <h1>GraphQL + React</h1>
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "center",
-                    }}
-                  >
-                    <Persons persons={data?.allPersons} />
-                  </div>
-                </>
-              )}
-            </div>
-            <div style={{ display: "flex", justifyContent: "space-around" }}>
-              <PhoneForm notifyError={notifyError} />
-              <PersonForm notifyError={notifyError} />
-            </div>
-          </div>
-        ) : (
-          <>
-            <h1>Iniciar Secion</h1>
-            <LoginForm setToken={setToken} notifyError={notifyError} />
-          </>
-        )}
-      </header> */}
     </div>
   );
 }
